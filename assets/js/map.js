52d797e75d780e744f6cdb1ed13dec94c1792e85
Zachsteuer98 <Zach.Steuer98@gmail.com>
2022-01-11 17:30:09 -0500

    added markers to map

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,25 +1,18 @@
-// var city = Ludlow
-
-// //Code for map and fetch
-
-// var getUserCity = function(city) {
-//     //format the URL
-//     var apiUrl = "http://api.powderlin.es/stations"
-// //make a request to the URL
-// fetch(apiUrl).then(function(response) {
-//     response.json().then(function(data) {
-//         console.log(data);
-// });
-// });
-// };
-
-getMountainData()
-
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
+    
+    const bearRiverResort = {lat: 30.8852, lng: -110.8277}
+    
+
+    const map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 41.43, lng: -101.01},
         zoom: 4
 })
+
+new google.maps.Marker({
+position: bearRiverResort,
+map,
+title: "Bear River"
+});
 }
 
 initMap()
