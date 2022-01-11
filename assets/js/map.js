89463134f115f50661c39a2d6cf4a140f16ee266
Zachsteuer98 <Zach.Steuer98@gmail.com>
2022-01-11 17:42:52 -0500

    added markers, need to make them accesible with an event listener

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,6 +1,7 @@
 function initMap() {
     
     const bearRiverResort = {lat: 30.8852, lng: -110.8277}
+    const beaverCreek = {lat: 39.60498, lng: -106.51645}
     
 
     const map = new google.maps.Map(document.getElementById('map'), {
@@ -13,6 +14,13 @@ position: bearRiverResort,
 map,
 title: "Bear River"
 });
+
+new google.maps.Marker({
+    position: beaverCreek,
+    map,
+    title: "Beaver Creek",
+  });
+
 }
 
 initMap()
