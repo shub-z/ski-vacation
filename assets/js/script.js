ffa317d51c07995698f64e94309d0560c3362423
Sean <mckeags7@gmail.com>
2022-01-11 19:58:52 -0500

    commit
    got rid of UV index script

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,30 +22,6 @@ $(document).ready(function () {
   
     const storedMountains = () =>
         localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
-
-    const uvCall = (lon, lat) => {
-        let uvQueryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&units=imperial&appid=77cb488591d883bec900753d1136d81c`;
-        
-        $.ajax({
-            url: uvQueryURL,
-            method: 'GET',
-        }).then(function (uvResponse) {
-            
-            $('#uvData').html(`${uvResponse.value}`);
-            
-            if (uvResponse.value <= 2) {
-                $('.uvRow').css('background-color', 'green');
-            } else if (uvResponse.value > 2 && uvResponse.value <= 5) {
-                $('.uvRow').css('background-color', 'yellow');
-            } else if (uvResponse.value > 5 && uvResponse.value <= 7) {
-                $('.uvRow').css('background-color', 'orange');
-            } else if (uvResponse.value > 7 && uvResponse.value <= 10) {
-                $('.uvRow').css('background-color', 'red');
-            } else {
-                $('.uvRow').css('background-color', 'violet');
-            }
-        });
-    };
   
     const fiveDay = (lon, lat) => {
         let fiveQueryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&units=imperial&appid=77cb488591d883bec900753d1136d81c`;
@@ -65,10 +41,7 @@ $(document).ready(function () {
                     `Temp: ${fiveResponse.daily[k].temp.day} &#8457;`
                 );
                 
-                $(`#${k}humid`).html(
-                    `Humidity: ${fiveResponse.daily[k].humidity}%`
-                );
-            }
+            };
         });
     };
     
